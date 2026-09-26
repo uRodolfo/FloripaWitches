f9996b0dac14bab75bb37a4a0d87041ddb285c5d
NFunchal <nicolas.funchal@gmail.com>
2026-09-26 06:46:20 -0300

    added particles

--- Scripts/player_bullet.gd
+++ Scripts/player_bullet.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal collided()
+
 var direction := Vector2.ZERO
 var speed : float
 
@@ -11,4 +13,5 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer or body.is_in_group("Enemy"):
+		collided.emit()
 		queue_free()
